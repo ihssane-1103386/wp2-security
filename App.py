d997dc1b2663523b659e5456a2ac7bf21d4d6791
Julie 1010439 <1010439@hr.nl>
2024-11-13 14:42:10 +0100

    Test.html wil niet echt meewerken, ben een oplossing aan het zoeken, waarom het bestand niet gevonden kan worden.

--- App.py
+++ App.py
@@ -3,16 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    print(request.args.get("test"))
-    print(request.args.get("Lastname"))
-    print(request.args.get("Age"))
-    print(request.args.get("Birthdate"))
-    print(request.args.get("email"))
-    print(request.args.get("Password"))
-    return render_template('hello_world.html')
+def test_file():  # put application's code here
+    print(request.args.get("voorbeeld"))
+    return render_template('Test.html')
 
 
 if __name__ == '__main__':
