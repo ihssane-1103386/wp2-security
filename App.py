1390d9805bdce189f98f16a5d7f3bf49d2b62320
Julie 1010439 <1010439@hr.nl>
2024-11-13 17:03:55 +0100

    Directory (templates) gevonden, en paars gemaakt. Flask run toegevoegd, maar er blijkt toch iets anders dat niet klopt.

--- App.py
+++ App.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 from flask import render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-@app.route('/')
-def test_file():  # put application's code here
-    print(request.args.get("voorbeeld"))
-    return render_template('/templates/Test.html')
+@app.route("/")
+def test_file():
+    print(request.args.get("firstname"))
+    return render_template('Test.html')
 
 
 if __name__ == '__main__':
