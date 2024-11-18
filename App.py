ffca0cf754d7b0cf1f1744d406fc32ec0f4bca89
Julie 1010439 <1010439@hr.nl>
2024-11-18 17:38:57 +0100

    Toetsvragen page

--- App.py
+++ App.py
@@ -8,6 +8,11 @@ def test_file():
     print(request.args.get("firstname"))
     return render_template('Test.html')
 
+def toetsvragen():
+    print(request.args.get("naam zoeken in json file"))
+    print(request.args.get("categorie zoeken in json file"))
+    print(request.args.get("resultaten zoeken met/zonder taxonomie"))
+    return render_template('toetsvragen.html')
 
 if __name__ == '__main__':
     app.run()
