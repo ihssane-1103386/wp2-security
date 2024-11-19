33b3d4fa02ab03bcb75d34336156ebb41227372f
Julie 1010439 <1010439@hr.nl>
2024-11-19 20:54:52 +0100

    Toetsvragen page update

--- App.py
+++ App.py
@@ -4,9 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def test_file():
-    print(request.args.get("firstname"))
-    return render_template('Test.html')
+# def test_file():
+#     print(request.args.get("firstname"))
+#     return render_template('Test.html')
 
 def toetsvragen():
     print(request.args.get("naam zoeken in json file"))
