914692ba4a0775e9d89aeed326a8c3bf7f95835a
julie <1010439@hr.nl>
2024-11-20 14:10:27 +0100

    Toetsvragen page update

--- App.py
+++ App.py
@@ -11,10 +11,12 @@ def inlog():
 def redacteur():
     return render_template('redacteur.html')
 
+@app.route("/toetsvragen")
 def toetsvragen():
     print(request.args.get("naam zoeken in json file"))
     print(request.args.get("categorie zoeken in json file"))
     print(request.args.get("resultaten zoeken met/zonder taxonomie"))
+    print(request.args.get("10 questions from json file"))
     return render_template('toetsvragen.html')
 
 if __name__ == '__main__':
