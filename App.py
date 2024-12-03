6dafe6292f4ab196bef1239e86e30eb0160e8279
Julie 1010439 <1010439@hr.nl>
2024-12-03 20:03:46 +0100

    Toetsvragen van database, staan nu op de html website. Nu alleen nog de website stijl verbeteren, en de indexeer buttons laten werken.

--- App.py
+++ App.py
@@ -1,27 +1,31 @@
-import json, os
+import json, sqlite3
 from flask import Flask
-from flask import render_template, request, redirect, url_for, jsonify
+from flask import render_template, request
+import json
 
 app = Flask(__name__)
 
 @app.route("/", endpoint="toetsvragen")
 def toetsvragen():
     try:
-        with open('static/assets/json files/questions_extract.json') as f:
-          vragen = json.load(f)
+        conn = sqlite3.connect('databases/database_toetsvragen.db')
+        cursor = conn.cursor()
 
-          page = int(request.args.get('page', 1))
+        page = int(request.args.get('page', 1))
 
-          per_page = 10
-          start = (page - 1) * per_page
-          end = start + per_page
+        per_page = 10
+        start = (page - 1) * per_page
+        end = start + per_page
 
-          vragen_pagina = vragen[start:end]
+        cursor.execute("SELECT question, vak, date_created FROM questions LIMIT ? OFFSET ?", (per_page,start))
+        vragen_pagina = cursor.fetchall()
 
-          next_page = page + 1 if end < len(vragen) else None
-          prev_page = page - 1 if start > 0 else None
+        cursor.execute("SELECT COUNT(*) FROM questions")
+        total_vragen = cursor.fetchone()[0]
+        total_pages = (total_vragen + per_page - 1) // per_page
 
-          total_pages = (len(vragen) + per_page - 1) // per_page
+        next_page = page + 1 if end < total_vragen else None
+        prev_page = page - 1 if start > 0 else None
 
         return render_template('toetsvragen.html', vragen=vragen_pagina,
                                page=page, next_page=next_page, prev_page=prev_page, total_pages=total_pages)
