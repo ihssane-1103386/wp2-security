f4fe9cb56385f698227fb32bb74d788c1818c09b
Julie 1010439 <1010439@hr.nl>
2024-12-08 00:12:27 +0100

    De toetsvragen connecten met indexeren en taxonomie wijzigen.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, request
 from flask import render_template
 
@@ -21,7 +22,22 @@ def vraag_taxonomie_resultaat():
 
 @app.route("/indexeren")
 def indexeren():
-    return render_template('vraag indexeren naar taxonomie.html')
+    vraag_id = request.args.get('vraag_id')
+    conn = sqlite3.connect('databases/database_toetsvragen.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM questions WHERE id = ?", (vraag_id,))
+    question = cursor.fetchone()
+    return render_template('vraag indexeren naar taxonomie.html', question=question)
+
+@app.route("/taxonomie_wijzigen")
+def taxonomie_wijzigen():
+    vraag_id = request.args.get('vraag_id')
+    conn = sqlite3.connect('databases/database_toetsvragen.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM questions WHERE id = ?", (vraag_id,))
+    question = cursor.fetchone()
+
+    return render_template('vraag geïndexeerd wijzigen.html', question=question)
 
 @app.route("/wijzig")
 def wijzig():
