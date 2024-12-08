926f5deb900562f4a0249f8898c2be8601d0297c
yoshua-1045100 <1045100@hr.nl>
2024-12-08 20:32:03 +0100

    SQL naar ander bestand verplaatst

---
+++ db_prompt_data.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def verbinding_maken_database():
+    conn = sqlite3.connect('databases/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def prompts_ophalen():
+    conn = verbinding_maken_database()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT 
+                        prompts.prompts_id, 
+                        prompts.prompt, 
+                        users.display_name AS user_display_name, 
+                        prompts.questions_count, 
+                        prompts.questions_correct
+                      FROM prompts
+                      JOIN users ON prompts.user_id = users.user_id''')
+    prompts = cursor.fetchall()
+    conn.close()
+    return prompts
+
+def prompt_details_ophalen(prompts_id):
+    conn = verbinding_maken_database()
+    cursor = conn.cursor()
+    cursor.execute('''SELECT 
+                        prompts.prompts_id, 
+                        prompts.prompt, 
+                        prompts.prompt_details, 
+                        users.display_name AS user_display_name, 
+                        prompts.questions_count, 
+                        prompts.questions_correct, 
+                        prompts.questions_incorrect, 
+                        prompts.date_created
+                      FROM prompts
+                      JOIN users ON prompts.user_id = users.user_id
+                      WHERE prompts.prompts_id = ?''', (prompts_id,))
+    prompt = cursor.fetchone()
+    conn.close()
+    return prompt
+
+def prompt_verwijderen(prompts_id):
+    conn = verbinding_maken_database()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM prompts WHERE prompts_id = ?', (prompts_id,))
+    conn.commit()
+    conn.close()
