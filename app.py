f51c1bea0d48cbfbd4194881a98451fb894275d1
Julie 1010439 <1010439@hr.nl>
2024-12-08 04:22:02 +0100

    html & app aangepast

--- app.py
+++ app.py
@@ -100,17 +100,22 @@ def vraag_taxonomie_resultaat():
 @app.route("/indexeren")
 def indexeren():
     vraag_id = request.args.get('vraag_id')
+
     conn = sqlite3.connect('databases/database_toetsvragen.db')
     cursor = conn.cursor()
+
     cursor.execute("SELECT * FROM questions WHERE id = ?", (vraag_id,))
     question = cursor.fetchone()
+
     return render_template('vraag indexeren naar taxonomie.html', question=question)
 
 @app.route("/taxonomie_wijzigen")
 def taxonomie_wijzigen():
     vraag_id = request.args.get('vraag_id')
+
     conn = sqlite3.connect('databases/database_toetsvragen.db')
     cursor = conn.cursor()
+
     cursor.execute("SELECT * FROM questions WHERE id = ?", (vraag_id,))
     question = cursor.fetchone()
 
