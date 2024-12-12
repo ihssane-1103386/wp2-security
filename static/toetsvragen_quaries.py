1333db10a7156ebdc6a027230cccc40eba6565ea
Julie 1010439 <1010439@hr.nl>
2024-12-12 18:24:27 +0100

    moving the quaries out of app.py

--- static/toetsvragen_quaries.py
+++ static/toetsvragen_quaries.py
@@ -10,10 +10,22 @@ count_query = ("SELECT COUNT(*) FROM questions WHERE 1=1")
 cursor.execute("SELECT DISTINCT vak FROM questions")
 
 def load_queries(path):
-    queries = []
+    queries = {}
     query_name = None
     parameters = []
 
     with open(path, 'r') as file:
         for line in file:
             line = line.strip()
+            if line.startswith('-[') and line.endswith(']'):
+                if query_name and parameters:
+                    queries[query_name] = ' '.join(parameters)
+                query_name = line[4:-1]
+                parameters = []
+            elif query_name:
+                parameters.append(line)
+
+        if query_name and parameters:
+            queries[query_name] = ' '.join(parameters)
+
+    return queries
