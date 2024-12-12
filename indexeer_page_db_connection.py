6c26ac290ed89353ecabd34d4e8593984ebf412b
Jordi-1111805 <1111805@hr.nl>
2024-12-12 10:42:06 +0100

    changed the database to current database 'database_toetsvragen'

--- indexeer_page_db_connection.py
+++ indexeer_page_db_connection.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def prompt_lijst():
-    conn = sqlite3.connect('databases/database.db')
+    conn = sqlite3.connect('databases/database_toetsvragen.db')
     cursor = conn.cursor()
     cursor = cursor.execute('''SELECT 
                             prompts.prompts_id, 
