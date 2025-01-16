3762494476c53fe13cc79b996e96309e26c2b589
Anmol Haribhajan <1096493@hr.nl>
2025-01-16 10:56:17 +0100

    SQL code added to query for inlog user data

--- static/queries.sql
+++ static/queries.sql
@@ -1,5 +1,8 @@
 -- Toetsvragen queries
 
+-- [login_query]
+SELECT * FROM users WHERE login = ? AND password = ?,(ingevulde_gebruikersnaam, ingevulde_wachtwoord)
+
 -- [normal_query]
 SELECT questions_id, question, vak, date_created, taxonomy_bloom FROM questions WHERE 1=1;
 
