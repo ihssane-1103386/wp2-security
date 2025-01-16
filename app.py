99bbd6a0230c90a5f847c9514de8540494932471
ihssane_1103386 <1103386@hr.nl>
2025-01-16 20:37:34 +0100

    hashed wachtwoord toepassen in inlog

--- app.py
+++ app.py
@@ -58,7 +58,8 @@ def inlog():
 
         if user:
             hashed_wachtwoord = user[2]
-            if bcrypt.check_password_hash(hashed_wachtwoord, ingevulde_wachtwoord):
+
+        if bcrypt.check_password_hash(hashed_wachtwoord, ingevulde_wachtwoord):
 
             # Zet de gebruiker in de sessie
             session['current_user'] = {
@@ -69,7 +70,7 @@ def inlog():
             }
             display_name = user[3]
             flash(f"Welkom {display_name}! Je bent succesvol ingelogd!", "success")
-            return redirect(url_for('toetsvragen'))
+            return render_template('toetsvragen.html.jinja')
         else:
             flash("Onjuiste gebruikersnaam of wachtwoord. Probeer het opnieuw.", "error")
         return render_template('inloggen.html.jinja')
